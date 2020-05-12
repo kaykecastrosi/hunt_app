f7e2e2ea3f69976225c3ca3de7008eef5c2c18f6
Kayke Castro <kaykecastrosi@gmail.com>
2020-05-12 14:39:53 -0300

    created index.js and changed main entry as it

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Template padrão Expo!</Text>
+        <View style={styles.box} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
-});
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#F00"
+  }
+})
